b06b6ebbab9e42fb5a8036f0d1b6c10b94033e56
bearodit@gmail.com <bearodit@gmail.com>
2025-02-07 23:23:17 +0200

    123

--- С++ Hw 30.0/С++ Hw 30.0.cpp
+++ С++ Hw 30.0/С++ Hw 30.0.cpp
@@ -1,20 +1,103 @@
-﻿// С++ Hw 30.0.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <clocale>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Person {
+	string name;
+	string surname;
+public:
+	Person(string name, string surname) {
+		this->surname = surname;
+		this->name = name;
+	}
+	Person() {
+		this->surname = surname;
+		this->name = name;
+	}
+	void Print() {
+		cout << "name: " << name << endl;
+		cout << "surname: " << surname << endl;
+	}
+	void SetName(string name) {
+		this->name = name;
+	}
+	void SetSurname(string surname) {
+		this->surname = surname;
+	}
+	string GetName() {
+		return name;
+	}
+	string GetSurname() {
+		return surname;
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	map<string, string> surnames{
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"},
+		{"Иванов", "Ivanov"}
+	};
+
+	map<string, string> names{
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"},
+		{"Иван", "Ivan"}
+	};
+
+	vector<Person> persons = {
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов"),
+		Person("Иван", "Иванов")
+	};
+
+	for (int i = 0; i < 15; i++) {
+		persons[i].SetSurname(surnames[persons[i].GetSurname()]);
+		persons[i].SetName(names[persons[i].GetName()]);
+	}
+	
+	for (int i = 0; i < 15; i++) {
+		persons[i].Print();
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
